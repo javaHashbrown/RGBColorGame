f7210856a1562f678dd1b019ccd5ee67362f68be
javaHashbrown <{javaHashbrown}@users.noreply.github.com>
2019-02-19 00:51:02 +0800

    initial boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import "../styles/App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>HoHoHo!!! My First React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
